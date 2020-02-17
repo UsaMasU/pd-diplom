52e282fa61222b109a516912779d83a913500227
UsaMasU <v_v_semenov@mail.ru>
2020-02-17 05:40:42 +0300

    19022020_2345

---
+++ orders/backend/serializers.py
@@ -0,0 +1,4 @@
+from rest_framework import serializers
+from .models import Shop, Category, Product, ProductInfo, Parameter, ProductParameter, Contact, Order, OrderItem
+
+
--- orders/orders/settings.py
+++ orders/orders/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'backend.apps.BackendConfig'
 ]
 
@@ -101,6 +102,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+
 
 
 # Internationalization
