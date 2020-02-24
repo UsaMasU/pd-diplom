014c964cd18a0af5fd1dc56fed44bc418434f743
UsaMasU <v_v_semenov@mail.ru>
2020-02-24 20:40:19 +0300

    27022020_0055

--- orders/backend/admin.py
+++ orders/backend/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Shop
 
-# Register your models here.
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ('name', 'url', 'user', 'state')
+    list_filter = ('name', 'user')
+    search_fields = ('name', 'user')
+    #prepopulated_fields = {'slug': ('name',)}
+    #ordering = ('name', 'id')
